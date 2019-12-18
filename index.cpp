af1a2b67e06ee2f99a05606897dc41da20160486
jamiematthews1 <jamie.puppyann@gmail.com>
2019-12-18 14:00:39 -0500

    formula parser added

--- index.cpp
+++ index.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <utility>
 #include <algorithm>
+#include <fstream>
 
 using std::vector, std::set, std::string, std::unordered_map, std::pair;
 
@@ -15,7 +16,7 @@ class Index {
   unordered_map<string, int> geneNamesToIdx;
   unordered_map<string, int> cellNamesToIdx;
   vector<pair<set<int>, set<int>>> expressed; // stores which cells express a given gene or don't; TODO: should they be ints?
-  
+
 public:
   Index(const vector<vector<bool>>& mat, const vector<string>& geneNames, const vector<string>& cellNames): matrix(mat), geneNames(geneNames), cellNames(cellNames), expressed(geneNames.size()) {
     assert(matrix.size() == geneNames.size());
@@ -32,4 +33,61 @@ public:
 	(matrix[i][j] ? expressed[i].first : expressed[i].second).insert(j);
     }
   }
+
+  int parse_helper(vector<string> *vec, string line, const char * delim) {
+      char cstr[line.size()+1];
+      strcpy(cstr, line.c_str());
+      char *token = strtok(cstr, delim);
+
+      short count = 0;
+      short size = 8;
+      (*vec).resize(size);
+
+      while (token != NULL) {
+          (*vec)[count] = token;
+          token = strtok(NULL, delim);
+          count++;
+          if (count == size) {
+              size = size*2;
+              (*vec).resize(size);
+          }
+      }
+
+      return count;
+  }
+
+  vector<vector<string> > build_formula(string input_file) {
+      ifstream input(input_file);
+      string line;
+      char *token;
+      vector<string> clauses;
+      vector<vector<string> > formula;
+
+      getline(input, line);
+      short num_clauses = parse_helper(&clauses, line, "^");
+      cout << "num clauses: " << num_clauses << "\n";
+      formula.resize(num_clauses);
+
+      // parse each clause into  literals
+      short num_literals;
+      for (int i=0; i < num_clauses; i++) {
+          num_literals = parse_helper(&(formula[i]), clauses[i], "|");
+          formula[i].resize(num_literals);
+      }
+
+      input.close();
+          return formula;
+  }
 };
+
+int main(int argc, char** argv)
+{
+    if (argc >= 2) {
+        if (((string)"query").compare(argv[1]) == 0) {
+            build_formula(argv[2]);
+        } else {
+            cout << "Unknown command. Please use query.";
+        }
+    }
+
+}
