9210d8ddbca9a8d539e1a61d8ebb1e1e1265a04c
jamiematthews1 <jamie.puppyann@gmail.com>
2019-12-18 20:54:02 -0500

    Add improved lookup

--- index.cpp
+++ index.cpp
@@ -36,60 +36,47 @@ public:
     }
   }
 
-  static int parse_helper(vector<string>& vec, string line, const char * delim) {
-      char cstr[line.size()+1];
-      strcpy(cstr, line.c_str());
-      char *token = strtok(cstr, delim);
-
-      short count = 0;
-      short size = 8;
-      vec.resize(size);
-
-      while (token != NULL) {
-          vec[count] = token;
-          token = strtok(NULL, delim);
-          ++count;
-          if (count == size) {
-              size *= 2;
-              vec.resize(size);
+  vector<int>* clause_helper(vector<pair<bool,string> > *clause, vector<int> *satisfied,
+      vector<int> *not_satisfied, int curr_index) {
+      // if we have iterated through all literals, then we are done
+      if (curr_index >= (*clause).size()) {
+          return satisfied;
+      }
+      else {
+          vector<int> still_unsatisfied;
+          int curr_gene = geneNamesToIdx[(*clause)[curr_index].second];
+          for(int i=0; i < (*not_satisfied).size(); i++){ //iterate over each cell
+              bool flag = (*clause)[curr_index].first;
+              bool cell = matrix[curr_gene][(*not_satisfied)[i]];
+              if ((cell&flag)|(!cell&!flag)) {
+                  (*satisfied).push_back((*not_satisfied)[i]);
+              } else {
+                  still_unsatisfied.push_back((*not_satisfied)[i]);
+              }
           }
+          return clause_helper(clause, satisfied, &still_unsatisfied, curr_index+1);
       }
-
-      return count;
   }
 
-  static vector<vector<string> > build_formula(string input_file) {
-      ifstream input(input_file);
-      string line;
-      char *token;
-      vector<string> clauses;
-      vector<vector<string> > formula;
-
-      getline(input, line);
-      short num_clauses = parse_helper(clauses, line, "^");
-      cout << "num clauses: " << num_clauses << "\n";
-      formula.resize(num_clauses);
-
-      // parse each clause into literals
-      short num_literals;
-      for (int i=0; i < num_clauses; i++) {
-          num_literals = parse_helper(formula[i], clauses[i], "|");
-          formula[i].resize(num_literals);
+  vector<int> next_clause(vector<vector<pair<bool,string> > > *formula, vector<int> *prev_set, int curr) {
+      // if we have iterated through all clauses, then we are done
+      if (curr >= (*formula).size()) {
+          return *prev_set;
       }
-
-      input.close();
-      return formula;
+      else {
+          vector<int> temp;
+          vector<int> *satisfied = clause_helper(&((*formula)[curr]), &temp, prev_set, 0);
+          return next_clause(formula, satisfied, curr+1);
+      }
   }
+
+  vector<int> first_clause(vector<vector<pair<bool,string> > > *formula) {
+      int gene = geneNamesToIdx[(*formula)[0][0].second];
+      vector<int> curr_sat = ((*formula)[0][0].first ? expressed[gene].first : expressed[gene].second);
+      vector<int> curr_unsat = ((*formula)[0][0].first ? expressed[gene].second : expressed[gene].first);
+      vector<int> temp;
+      curr_sat = *(clause_helper(&((*formula)[0]), &curr_sat, &curr_unsat, 1));
+      return next_clause(formula, &curr_sat, 1);
+  }
+
 };
-
-int main(int argc, char** argv)
-{
-    if (argc >= 2) {
-        if (((string)"query").compare(argv[1]) == 0) {
-	  Index::build_formula(argv[2]);
-        } else {
-            cout << "Unknown command. Please use query.";
-        }
-    }
-
-}
