4af22b2691181ae95e871f41eeaeedfae7797269
jamiematthews1 <jamie.puppyann@gmail.com>
2019-12-19 13:41:25 -0500

    changed not probabilty

--- makeForms.cpp
+++ makeForms.cpp
@@ -20,10 +20,8 @@ int main(int argc, char** argv)
     string output = "forms.txt";
     int num_forms = 10;
     int num_clauses = 5;
-    //int clause_low = 1;
     int clause_high = 10;
-    //int max_gene = 2000; //not inclusive
-    double not_prob = 0.5;
+    int not_prob = 2; //not probability is 1 / this number
 
     vector<vector<bool> > matrix; // genes are rows, cells are columns
     vector<string> cellNames, geneNames;
@@ -42,7 +40,7 @@ int main(int argc, char** argv)
          for (int j=0; j<num_clauses; j++) {
              int num_literals = (rand() % clause_high) + 1;
              for (int k=0; k<num_literals; k++) {
-                 if (rand()%2 == 1) {
+                 if (rand()%not_prob == 0) {
                      output_file << "!";
                  }
                  int next_lit = (rand() % max_gene);
